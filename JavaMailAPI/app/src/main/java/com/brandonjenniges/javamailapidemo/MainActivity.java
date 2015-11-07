85f0bc1d3ae67cd0c62f682c68cb7348cb05991d
Brandon Jenniges <steveholt55@gmail.com>
2015-11-07 16:29:31 -0600

    Moves project to gradle

---
+++ JavaMailAPI/app/src/main/java/com/brandonjenniges/javamailapidemo/MainActivity.java
@@ -0,0 +1,118 @@
+package com.brandonjenniges.javamailapidemo;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+import javax.mail.AuthenticationFailedException;
+import javax.mail.MessagingException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText user;
+    private EditText pass;
+    private EditText subject;
+    private EditText body;
+    private EditText recipient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendMessage();
+            }
+        });
+
+        user = (EditText) findViewById(R.id.username);
+        pass = (EditText) findViewById(R.id.password);
+        subject = (EditText) findViewById(R.id.subject);
+        body = (EditText) findViewById(R.id.body);
+        recipient = (EditText) findViewById(R.id.recipient);
+        
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void sendMessage() {
+        String[] recipients = { recipient.getText().toString() };
+        SendEmailAsyncTask email = new SendEmailAsyncTask();
+        email.activity = this;
+        email.m = new Mail(user.getText().toString(), pass.getText()
+                .toString());
+        email.m.set_from(user.getText().toString());
+        email.m.setBody(body.getText().toString());
+        email.m.set_to(recipients);
+        email.m.set_subject(subject.getText().toString());
+        email.execute();
+    }
+
+    public void displayMessage(String message) {
+        Snackbar.make(findViewById(R.id.fab), message, Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+    }
+}
+
+class SendEmailAsyncTask extends AsyncTask<Void, Void, Boolean> {
+    Mail m;
+    MainActivity activity;
+
+    public SendEmailAsyncTask() {}
+
+    @Override
+    protected Boolean doInBackground(Void... params) {
+        try {
+            if (m.send()) {
+                activity.displayMessage("Email sent.");
+            } else {
+                activity.displayMessage("Email failed to send.");
+            }
+
+            return true;
+        } catch (AuthenticationFailedException e) {
+            Log.e(SendEmailAsyncTask.class.getName(), "Bad account details");
+            e.printStackTrace();
+            activity.displayMessage("Authentication failed.");
+            return false;
+        } catch (MessagingException e) {
+            Log.e(SendEmailAsyncTask.class.getName(), "Email failed");
+            e.printStackTrace();
+            activity.displayMessage("Email failed to send.");
+            return false;
+        } catch (Exception e) {
+            e.printStackTrace();
+            activity.displayMessage("Unexpected error occured.");
+            return false;
+        }
+    }
+}
